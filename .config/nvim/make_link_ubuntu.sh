5cfbb98c3aca4cb1fe0b590408d232cb04a441a4
kazu-gor <ik126@icloud.com>
2024-08-28 12:25:04 +0900

    🔧 (make_link_ubuntu.sh): create symbolic links for neovim configuration files and plugins to organize and manage neovim setup efficiently

---
+++ .config/nvim/make_link_ubuntu.sh
@@ -0,0 +1,23 @@
+mkdir -p ~/.config/nvim/lua/config
+mkdir -p ~/.config/nvim/lua/plugins
+
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/airline.lua ~/.config/nvim/lua/plugins/airline.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/airline_theme.lua ~/.config/nvim/lua/plugins/airline_theme.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/completion.lua ~/.config/nvim/lua/plugins/completion.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/copilot.lua ~/.config/nvim/lua/plugins/copilot.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/easymotion.lua ~/.config/nvim/lua/plugins/easymotion.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/fugitive.lua ~/.config/nvim/lua/plugins/fugitive.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/fzf.lua ~/.config/nvim/lua/plugins/fzf.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/gitgutter.lua ~/.config/nvim/lua/plugins/gitgutter.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/jumpcursor.lua ~/.config/nvim/lua/plugins/jumpcursor.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/lsp.lua ~/.config/nvim/lua/plugins/lsp.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/smoothcursor.lua ~/.config/nvim/lua/plugins/smoothcursor.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/sneak.lua ~/.config/nvim/lua/plugins/sneak.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/tagbar.lua ~/.config/nvim/lua/plugins/tagbar.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/tree.lua ~/.config/nvim/lua/plugins/tree.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/diffview.lua ~/.config/nvim/lua/plugins/diffview.lua
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lua/plugins/toggleterm.lua ~/.config/nvim/lua/plugins/toggleterm.lua
+
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
+
+ln -fnsv ~/git/migration/dotfiles/.config/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json
